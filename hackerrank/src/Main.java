204c00bed6402ab0f276c5f5a93c6374ba30d3c0
Thien Nguyen <crowclow_ZZOS@yahoo.com>
2017-07-14 22:48:43 +0700

    add first folder on github

---
+++ hackerrank/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by thien on 14/07/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
